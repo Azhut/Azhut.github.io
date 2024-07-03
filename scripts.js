2efc4bb55f10b2fabad6c6e7ff0a7cb28e59492c
Zhou_zhi <zhora.klem@yandex.ru>
2024-07-03 10:05:59 +0500

    add pdf

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@ function showDocument(doc) {
     
     switch(doc) {
         case 'doc1':
-            content.innerHTML = '<h2>Документ 1</h2><p>Это содержание документа 1.</p>';
+            content.innerHTML = '<iframe src="documents/doc1.pdf" frameborder="0" width="100%" height="600px"></iframe>';
             break;
         case 'doc2':
-            content.innerHTML = '<h2>Документ 2</h2><p>Это содержание документа 2.</p>';
+            content.innerHTML = '<iframe src="documents/doc2.pdf" frameborder="0" width="100%" height="600px"></iframe>';
             break;
         case 'doc3':
-            content.innerHTML = '<h2>Документ 3</h2><p>Это содержание документа 3.</p>';
+            content.innerHTML = '<iframe src="documents/doc3.pdf" frameborder="0" width="100%" height="600px"></iframe>';
             break;
         default:
             content.innerHTML = '<p>Выберите документ для просмотра.</p>';
